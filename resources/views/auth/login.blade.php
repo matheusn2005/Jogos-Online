1bd811cf327dde634dfd842b95ae29a194705b8c
Aluno IFSC <ifsc@CDR-772578.cacador.ifsc.edu.br>
2025-04-23 22:18:17 -0300

    ajustes no css do register e login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,9 +2,10 @@
 <html>
 <head>
     <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
 </head>
 <body>
-    <h2>Login do Cliente</h2>
+
 
     @if ($errors->any())
         <div style="color: red;">
@@ -15,16 +16,27 @@
             </ul>
         </div>
     @endif
+<div class="d-flex justify-content-center m-5">
+  <div class="row justify-content-center">
+    <div class="col">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h2 class="card-title">Login do Cliente</h2>
+                    <form class="mb-3" method="POST" action="{{ route('login') }}">
+                        @csrf
+                        <label for="exampleFormControlInput1" class="form-label">Email:</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" name="email" required><br><br>
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-        <label>Email:</label>
-        <input type="email" name="email" required><br><br>
+                        <label for="exampleFormControlInput1" class="form-label">Senha:</label>
+                        <input type="password" class="form-control" id="exampleFormControlInput1" name="password" required><br><br>
 
-        <label>Senha:</label>
-        <input type="password" name="password" required><br><br>
-
-        <button type="submit">Entrar</button>
-    </form>
+                        <button class="btn btn-primary" type="submit">Entrar</button>
+                    </form> 
+            </div>
+        </div>
+    </div>
+  </div>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
 </body>
 </html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,9 +2,9 @@
 <html>
 <head>
     <title>Cadastro</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
 </head>
 <body>
-    <h2>Cadastro</h2>
 
     @if ($errors->any())
         <div style="color:red;">
@@ -16,22 +16,45 @@
         </div>
     @endif
 
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
 
-        <label>nome:</label>
-        <input type="text" name="name" required><br><br>
+<div class="d-flex justify-content-center m-5">
+  <div class="row justify-content-center">
+    <div class="col">
+        <div class="card" style="width: 28rem;">
+            <div class="card-body">
+                <h2 class="card-title">Registro do Cliente</h2>
+                <form method="POST" action="{{ route('register') }}">
+                    @csrf
+                    <div class="row">
+                        <div class="col-6">
+                            <label for="exampleFormControlInput1" class="form-label">Nome:</label>
+                            <input class="form-control" type="text" name="name" required><br><br>
+                        </div>
 
-        <label>Email:</label>
-        <input type="email" name="email" required><br><br>
+                        <div class="col-6">
+                            <label for="exampleFormControlInput1" class="form-label">Email:</label>
+                            <input class="form-control" type="email" name="email" required><br><br>
+                        </div>
 
-        <label>Senha:</label>
-        <input type="password" name="password" required><br><br>
+                        <div class="col-6">
+                            <label for="exampleFormControlInput1" class="form-label">Senha:</label>
+                            <input class="form-control" type="password" name="password" required><br><br>
+                        </div>
 
-        <label>Confirme a Senha:</label>
-        <input type="password" name="password_confirmation" required><br><br>
+                        <div class="col-6">
+                            <label for="exampleFormControlInput1" class="form-label">Confirme a Senha:</label>
+                            <input class="form-control" type="password" name="password_confirmation" required><br><br>
+                        </div> 
+                    </div>
+                    <button class="btn btn-primary" type="submit">Cadastrar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+  </div>
+</div>
 
-        <button type="submit">Cadastrar</button>
-    </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+
 </body>
 </html>
