2994422053e3f7c6754f2b04321ea110700d198d
matheusn2005 <matheus.n2005@aluno.ifsc,edu.br>
2025-05-07 21:58:25 -0300

    adição do google analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,17 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
+
+        <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-4F4C9RMGLB"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-4F4C9RMGLB');
+    </script>
+    
     <meta charset="UTF-8">
     <title>@yield('title', 'Jogos Online')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
